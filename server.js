b1af2e7066a8536b2818ff63c899e27f86f7e331
Andrea Di Giacomo <andrea.digiacomo@live.it>
2019-06-29 16:25:46 +0200

    change port

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ const path = require('path');
 
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
